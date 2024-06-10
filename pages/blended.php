9887d5b06d84ce42f361e2fbb81e4efabc7002b7
Minh Thai <moshi@moshi.fr>
2024-06-10 14:53:32 +0200

    fix instagram

--- pages/blended.php
+++ pages/blended.php
@@ -32,13 +32,13 @@
     <article class="flex flex-col lg:flex-row justify-evenly font-avenir max-lg:gap-10 max-lg:items-center">
 
         <div class="basis-1/2 px-14" data-aos="fade-down">
-            <img class="object-cover" src="/107-DSC02848%20-%20Theo%20Schuman%20(1)-min.png" alt="">
+            <img class="object-cover" src="/img/107-DSC02848%20-%20Theo%20Schuman%20(1)-min.png" alt="">
         </div>
 
         <div class="flex flex-col justify-around text-justify px-6 gap-10 text-xl basis-1/2">
             <div class="flex flex-col lg:flex-row gap-8 items-center">
                 <div class="basis-1/5"  data-aos="fade-down">
-                    <img src="/picto/tonneau.svg" alt="">
+                    <img src="/img/picto/tonneau.svg" alt="">
                 </div>
                 <div class="self-center basis-4/5"  data-aos="fade-down">
                     <p class="">Vieilli 3 ans dans de petits fûts de chêne américain, il révèle un profil aromatique à la fois
@@ -48,7 +48,7 @@
             </div>
             <div class="flex flex-col lg:flex-row gap-8 items-center">
                 <div class="basis-1/5" data-aos="fade-down">
-                    <img src="/picto/nez.svg" alt="">
+                    <img src="/img/picto/nez.svg" alt="">
                 </div>
                 <div class="self-center basis-4/5" data-aos="fade-down">
                     <p>Au nez, appréciez les notes fraîches de vanille.
@@ -57,7 +57,7 @@
             </div>
             <div class="flex flex-col lg:flex-row gap-8 items-center">
                 <div class="basis-1/5" data-aos="fade-down">
-                    <img src="/picto/bouche.svg" alt="">
+                    <img src="/img/picto/bouche.svg" alt="">
                 </div>
                 <div class="self-center basis-4/5" data-aos="fade-down">
                     <p>Très aromatique retrouvez des arômes chauds de pain cuit et d'oranges confites.
@@ -66,7 +66,7 @@
             </div>
             <div class="flex flex-col lg:flex-row gap-8 items-center">
                 <div class="basis-1/5" data-aos="fade-down">
-                    <img src="/picto/verre.svg" alt="">
+                    <img src="/img/picto/verre.svg" alt="">
                 </div>
                 <div class="self-center basis-4/5" data-aos="fade-down">
                     <p>À déguster pur ou dans de délicieux cocktails.
@@ -80,7 +80,7 @@
 
         <div class="w-full lg:basis-2/3 self-center line_decor" data-aos="fade-down">
             <img class="mx-auto relative z-10"
-                 src="/background/MACARON_LOGO_2_SANS_BORDURE_Plan_de_travail_1%201.svg" alt="">
+                 src="/img/background/MACARON_LOGO_2_SANS_BORDURE_Plan_de_travail_1%201.svg" alt="">
         </div>
 
         <div class="lg:basis-1/3 self-center mt-8 lg:mt-0 " data-aos="fade-down">
